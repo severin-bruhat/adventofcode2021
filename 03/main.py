01c9622891d1d68fb463cbf9d25e2b26c9febe76
Severin Bruhat <sbruhat@gmail.com>
2021-12-04 16:12:16 +0000

    add day 03 part 2

--- 03/main.py
+++ 03/main.py
@@ -26,6 +26,43 @@ def part_1(file):
 
     return int(gamma, 2) * int(epsilon, 2)
 
+######################## PART 2 ########################
+
+
+def filter(position, rows, bit):
+    rows0 = []
+    rows1 = []
+
+    if len(rows) == 1 or position == 12:
+        return rows[0]
+
+    for row in rows:
+        if row[position] == '0':
+            rows0.append(row)
+        elif row[position] == '1':
+            rows1.append(row)
+
+    if bit == '1':
+        return filter(position + 1, rows1 if len(rows1) >= len(rows0) else rows0, bit)
+    elif bit == '0':
+        return filter(position + 1, rows0 if len(rows0) <= len(rows1) else rows1, bit)
+
+
+def part_2(file):
+    diagnostic = read_file(file)
+
+    oxygen = filter(0, diagnostic, '1')
+    co2 = filter(0, diagnostic, '0')
+    life_support = int(oxygen, 2) * int(co2, 2)
+
+    print("oxygen", oxygen)
+    print("co2", co2)
+    print("life_support", life_support)
+
 
 print("PART 1")
-print(part_1("input-day03.txt")) #output: 738234
+print(part_1("input-day03.txt"))
+
+
+print("PART 2")
+print(part_2("input-day03.txt"))
